4dd074aec778152ae98072933d7fad738e54c084
Bartlomiej Mlynarkiewicz <bmlynarkiewicz@hotmail.com>
2020-03-05 18:20:53 +0000

    Test commit

--- index.php
+++ index.php
@@ -111,32 +111,12 @@ h1 {
 					</div>
 				
 					  <a class="waves-effect waves-light btn purple"><i class="material-icons left">play_arrow</i>Login</a>
-                      <a class="waves-effect waves-light btn green" onclick=""><i class="material-icons right">play_arrow</i>Register</a>
+                      <a class="waves-effect waves-light btn green" onclick="<?php
+                      $email = new Email("test@example.com", "bmlynarkiewicz1996@gmail.com", "Hello from iDate", "Test email from heroku");
+                      $email -> sendEmail();?>"><i class="material-icons right">play_arrow</i>Register</a>
                       <input type="submit" class="button" name="select" value="select"/>
 
-                      <?php
-                      if (isset($_POST['action'])) {
-                          switch ($_POST['action']) {
-                              case 'insert':
-                                  insert();
-                                  break;
-                              case 'select':
-                                  select();
-                                  break;
-                          }
-                      }
-
-                      function select() {
-                          $email = new Email("test@example.com", "bmlynarkiewicz1996@gmail.com", "Hello from iDate", "Test email from heroku");
-                          $email -> sendEmail();
-                          exit;
-                      }
-
-                      function insert() {
-                          echo "The insert function is called.";
-                          exit;
-                      }
-                      ?>
+                  
                   </div>
 			  <div class="col s6 center-align hide-on-small-only">
 				<img style="border-radius: 20px 20px 0px 0px;"  height="300px" width="100%" src="https://image.shutterstock.com/image-vector/vector-app-user-illustration-flat-600w-1229510083.jpg"/>
