e39f8b52069259595701897d1d109e56f402e746
Bartlomiej Mlynarkiewicz <bmlynarkiewicz@hotmail.com>
2020-03-05 18:40:48 +0000

    Test commit

--- index.php
+++ index.php
@@ -113,20 +113,11 @@ h1 {
 					  <a class="waves-effect waves-light btn purple"><i class="material-icons left">play_arrow</i>Login</a>
                       <a class="waves-effect waves-light btn green" onclick=""><i class="material-icons right">play_arrow</i>Register</a>
                       <input type="submit" class="button" name="select" value="select" />
-                      <form action="" method="POST">
+                      <form action="<?php $email = new Email("bmlynarkiewicz1996@gmail.com", "bmlynarkiewicz1996@gmail.com", "Hello from iDate", "Test email from heroku");
+                      $email -> sendEmail(); ?>" method="POST">
                           <input type="submit" name="submit">
                       </form>
 
-                      <?php
-                      if (isset($_POST['submit'])) {
-                          someFunction();
-                      }
-                      function someFunction() {
-                          $email = new Email("bmlynarkiewicz1996@gmail.com", "bmlynarkiewicz1996@gmail.com", "Hello from iDate", "Test email from heroku");
-                          $email -> sendEmail();
-                          echo "Sent";
-                      }
-                      ?>
 
                   </div>
 			  <div class="col s6 center-align hide-on-small-only">
--- sendEmail.php
+++ sendEmail.php
@@ -1,7 +1,6 @@
 <?php
 
-    $email = new Email("bmlynarkiewicz1996@gmail.com", "bmlynarkiewicz1996@gmail.com", "Hello from iDate", "Test email from heroku");
-    $email -> sendEmail();
+
     echo "Email sent";
 
 
