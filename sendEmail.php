7c2a4bc4b659220e32967da45b52e736f405ba17
Bartlomiej Mlynarkiewicz <bmlynarkiewicz@hotmail.com>
2020-03-05 18:34:35 +0000

    Test commit

--- sendEmail.php
+++ sendEmail.php
@@ -1,9 +1,7 @@
 <?php
 
-if(isset($_POST["submit"])){
-
     $email = new Email("bmlynarkiewicz1996@gmail.com", "bmlynarkiewicz1996@gmail.com", "Hello from iDate", "Test email from heroku");
     $email -> sendEmail();
+    echo "Email sent";
 
-}
 
