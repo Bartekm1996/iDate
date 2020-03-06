9399492ccdceb2e2803a8e34368774be30cff991
Bartlomiej Mlynarkiewicz <bmlynarkiewicz@hotmail.com>
2020-03-06 01:12:00 +0000

    Mongo connection

--- mongotest.php
+++ mongotest.php
@@ -1,8 +1,12 @@
 <?php
-$connectionString = "mongodb://idev.ie:27017";
+$connectionString = getenv("MONGO_URI");
 
 //$mongo = new MongoDB\Client($connectionString);
 //
 //$dbs = $mongo->listDatabases();
 //echo $dbs;
-$conn = new MongoClient($connectionString);
+$conn = new MongoDB\Driver\Manager($connectionString);
+
+$listdatabases = new MongoDB\Driver\Command(["listDatabases" => 1]);
+$res = $conn->executeCommand("admin", $listdatabases);
+echo $res->getServer()->getHost()."\n";
