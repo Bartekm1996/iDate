fc72a936f56e0b52403095b53eac8d0b387a5016
Bartlomiej Mlynarkiewicz <bmlynarkiewicz@hotmail.com>
2020-03-06 01:16:23 +0000

    Mongo connection

--- mongotest.php
+++ mongotest.php
@@ -1,12 +1,12 @@
 <?php
 $connectionString = getenv("MONGO_URI");
 
-//$mongo = new MongoDB\Client($connectionString);
-//
-//$dbs = $mongo->listDatabases();
-//echo $dbs;
 $conn = new MongoDB\Driver\Manager($connectionString);
 
 $listdatabases = new MongoDB\Driver\Command(["listDatabases" => 1]);
 $res = $conn->executeCommand("admin", $listdatabases);
-echo $res->getServer()->getHost()."\n";
+
+$result = $res->getServer()->getHost()."\n";
+$result = $res->getServer()->getInfo();
+print_r($result);
+
