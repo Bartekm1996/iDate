b247f4d49c2113ccf5ab02e28de49dc168981416
Paul Kinsella <paulkinselladev@gmail.com>
2020-03-06 14:04:03 +0000

    Login db test code

--- index.php
+++ index.php
@@ -48,7 +48,7 @@
                 <div class="container-login100-form-btn">
                     <div class="wrap-login100-form-btn">
                         <div class="login100-form-bgbtn"></div>
-                        <button class="login100-form-btn" onclick="">
+                        <button class="login100-form-btn">
                             Sign In
                         </button>
                     </div>
