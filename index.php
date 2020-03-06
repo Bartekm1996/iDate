9656719708e8b348fd6c5af65be1af5c2d569586
Bartlomiej Mlynarkiewicz <bmlynarkiewicz@hotmail.com>
2020-03-06 00:48:49 +0000

    Layout Change

--- index.php
+++ index.php
@@ -1,135 +1,151 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <title>Login V13</title>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>IDate</title>
-    <!-- JS -->
-	      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/png" href="images/icons/favicon.ico"/>
+    <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+    <link rel="stylesheet" type="text/css" href="fonts/iconic/css/material-design-iconic-font.min.css">
+    <link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+    <link rel="stylesheet" type="text/css" href="vendor/css-hamburgers/hamburgers.min.css">
+    <link rel="stylesheet" type="text/css" href="vendor/animsition/css/animsition.min.css">
+    <link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+    <link rel="stylesheet" type="text/css" href="vendor/daterangepicker/daterangepicker.css">
+    <link rel="stylesheet" type="text/css" href="css/util.css">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet"/>
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-    <!-- Compiled and minified CSS -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-
-    <!-- Compiled and minified JavaScript -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-
-			<script>
-			$(document).ready(function() {
-			  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.autocomplete');
-    var instances = M.Autocomplete.init(elems, options);
-  });
-  
-      $('input.autocomplete').autocomplete({
-      data: {
-        "Apple": null,
-        "Microsoft": null,
-        "Google": 'https://placehold.it/250x250'
-      },
-    });
-  });
-  </script>
-  
-              <style>
-			body {
-				background-image: url("images/bg.jpg");
-		}
-
-		.test {
-		margin-top:10%;
-  min-height: 400px;
-}
-.center {
-  margin: auto;
-  width: 50%;
-  padding: 10px;
-}
-.column {
-  float: left;
-  width: 50%;
-  padding: 10px;
-}
-
-
-/* Clear floats after the columns */
-.row:after {
-  content: "";
-  display: table;
-  clear: both;
-}
-
-
-@media screen and (max-width: 600px) {
-  .smallscreen {
-      display: none;
-   }
-   
-}
-
-h1 {
-        font-family: 'Lobster', serif;
-        font-size: 78px;
-      }
-			</style>
-
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lobster">
-		  
 </head>
-<body>
+<body style="background-color: #999999;">
 
-	<div class="container">
-        <nav><!-- Norbert We don't need a nav as left menu like in spec image is all we need. -->
-            <div class="nav-wrapper card-panel pink ">
-                <a href="#" class="brand-logo">iDate</a>
-                <ul id="nav-mobile" class="right hide-on-med-and-down">
-                    <li><a href="home.html">Home</a></li>
-                    <li><a href="confections.html">Confections</a></li>
-                    <li><a href="profile.html">Profile</a></li>
-                </ul>
-            </div>
-        </nav>
-  <div class="row">
-
-      <div style="border-radius: 30px 3px;" class="card test">
-        <div class="card-content center-align">
-				<h1 style="text-align:center">iDate</h1>
-			  <div class="row">
-			  
-				  <div class="col m6 s12">
-					<div class="input-field">
-					  <i class=	"material-icons prefix">email</i>
-					  <input type="text" id="autocomplete-input" class="autocomplete">
-					  <label for="autocomplete-input">Email</label>
-					</div>
-
-					<div class="input-field">
-					  <i class="material-icons prefix">lock</i>
-					  <input type="text" id="autocomplete-input" class="autocomplete">
-					  <label for="autocomplete-input">Passsword</label>
-					</div>
-				
-					  <a class="waves-effect waves-light btn purple"><i class="material-icons left">play_arrow</i>Login</a>
-                      <a class="waves-effect waves-light btn green" onclick=""><i class="material-icons right">play_arrow</i>Register</a>
-                      <input type="submit" class="button" name="select" value="select" />
-                   
-
-
-                  </div>
-			  <div class="col s6 center-align hide-on-small-only">
-				<img style="border-radius: 20px 20px 0px 0px;"  height="300px" width="100%" src="https://image.shutterstock.com/image-vector/vector-app-user-illustration-flat-600w-1229510083.jpg"/>
-			  </div>
-
-			</div>
+<div class="limiter">
+    <div class="container-login100">
+        <div class="login100-more" style="background-image: url('images/bg-01.jpg');">
         </div>
-      </div>
-   
-  </div>
+
+
+        <div class="wrap-login100 p-l-50 p-r-50 p-t-72 p-b-50">
+            <span class="login100-form-title p-b-59" id="signUpHeader">Sing In</span>
+
+            <form id="loginForm" class="login100-form validate-form" action="Login.php" >
+
+                <div class="wrap-input100 validate-input" data-validate="UserName / Email is required">
+                    <span class="label-input100">User Name</span>
+                    <input class="input100" type="text" name="user_name" placeholder="User Name / Email">
+                    <span class="focus-input100"></span>
+                </div>
+
+                <div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+                    <span class="label-input100">Email</span>
+                    <input class="input100" type="text" name="email" placeholder="Email addess...">
+                    <span class="focus-input100"></span>
+                </div>
+
+                <div class="container-login100-form-btn">
+                    <div class="wrap-login100-form-btn">
+                        <div class="login100-form-bgbtn"></div>
+                        <button class="login100-form-btn" onclick="">
+                            Sign In
+                        </button>
+                    </div>
+                </div>
+
+
+            </form>
+
+
+
+
+
+
+
+            <form id="registerForm" class="login100-form validate-form" action="Login.php" style="display: none;">
+
+                <div class="wrap-input100 validate-input" data-validate="Name is required">
+                    <span class="label-input100">Full Name</span>
+                    <input class="input100" type="text" name="name" placeholder="Name...">
+                    <span class="focus-input100"></span>
+                </div>
+
+                <div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
+                    <span class="label-input100">Email</span>
+                    <input class="input100" type="text" name="email" placeholder="Email addess...">
+                    <span class="focus-input100"></span>
+                </div>
+
+                <div class="wrap-input100 validate-input" data-validate="Username is required">
+                    <span class="label-input100">Username</span>
+                    <input class="input100" type="text" name="username" placeholder="Username...">
+                    <span class="focus-input100"></span>
+                </div>
+
+                <div class="wrap-input100 validate-input" data-validate = "Password is required">
+                    <span class="label-input100">Password</span>
+                    <input class="input100" type="text" name="pass" placeholder="*************">
+                    <span class="focus-input100"></span>
+                </div>
+
+                <div class="wrap-input100 validate-input" data-validate = "Repeat Password is required">
+                    <span class="label-input100">Repeat Password</span>
+                    <input class="input100" type="text" name="repeat-pass" placeholder="*************">
+                    <span class="focus-input100"></span>
+                </div>
+
+                <div class="flex-m w-full p-b-33">
+                    <div class="contact100-form-checkbox">
+                        <input class="input-checkbox100" id="ckb1" type="checkbox" name="remember-me">
+                        <label class="label-checkbox100" for="ckb1">
+								<span class="txt1">
+									I agree to the
+									<a href="#" class="txt2 hov1">
+										Terms of User
+									</a>
+								</span>
+                        </label>
+                    </div>
+
+
+                </div>
+
+                <div class="container-login100-form-btn">
+                    <div class="wrap-login100-form-btn">
+                        <div class="login100-form-bgbtn"></div>
+                        <button class="login100-form-btn" onclick="">
+                            Sign Up
+                        </button>
+                    </div>
+                </div>
+            </form>
+
+            <a href="#" id="signUp" class="dis-block txt3 hov1 p-r-30 p-t-10 p-b-10 p-l-30">
+                Sign Up
+                <i class="fa fa-long-arrow-right m-l-5"></i>
+            </a>
+
+            <p style="margin: auto;" align="middle">IDate</p>
+        </div>
+    </div>
 </div>
 
 
+</div>
+
+
+
+
+<script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+<script src="vendor/animsition/js/animsition.min.js"></script>
+<script src="vendor/bootstrap/js/popper.js"></script>
+<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+<script src="vendor/select2/select2.min.js"></script>
+<script src="vendor/daterangepicker/moment.min.js"></script>
+<script src="vendor/daterangepicker/daterangepicker.js"></script>
+<script src="vendor/countdowntime/countdowntime.js"></script>
+<script src="js/main.js"></script>
+
 </body>
 </html>
-
 
