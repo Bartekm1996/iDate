15664086bcb94ac408b3ecaf08030bce7f5d9dba
Bartlomiej Mlynarkiewicz <bmlynarkiewicz@hotmail.com>
2020-03-06 16:04:40 +0000

    Changed vendor -> vendorv

--- db.php
+++ db.php
@@ -1,8 +1,2 @@
  <?php
- //$conn = new mysqli(getenv("DB_URL"), getenv("DB_USER"), getenv("DB_PASS"), getenv("DB_SCHEMA"));
- $servername = "http://www.idev.ie";
- $username = "uldev";
- $password = "uldev@cs4116";
- $dbname = "cs4116";
-
- $conn = new mysqli($servername, $username, $password, $dbname);
+ $conn = new mysqli(getenv("DB_URL"), getenv("DB_USER"), getenv("DB_PASS"), getenv("DB_SCHEMA"));
