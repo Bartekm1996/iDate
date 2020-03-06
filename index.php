a372724c5daf6d20a2e08a2f3555cfaa59ecf7f4
Paul Kinsella <paulkinselladev@gmail.com>
2020-03-06 13:15:01 +0000

    Login db test code

--- index.php
+++ index.php
@@ -17,6 +17,7 @@
     <link rel="stylesheet" type="text/css" href="css/util.css">
     <link rel="stylesheet" type="text/css" href="css/main.css">
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet"/>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 
 </head>
 <body style="background-color: #999999;">
