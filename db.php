0eb19e6a4e9736cf407535e35e72b71a93c6e99b
Paul Kinsella <paulkinselladev@gmail.com>
2020-03-06 13:12:20 +0000

    Login db test code

--- db.php
+++ db.php
@@ -1,3 +1,3 @@
  <?php
- $conn = new mysqli(getenv("DB_URL"), getenv("DB_USER"), getenv("DB_PASSWORD"), getenv("DB_SCHEMA"));
+ $conn = new mysqli(getenv("DB_URL"), getenv("DB_USER"), getenv("DB_PASS"), getenv("DB_SCHEMA"));
 ?>
