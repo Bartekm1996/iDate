37338302ea14c022079d9d4bc6030a38bd7295e3
Paul Kinsella <paulkinselladev@gmail.com>
2020-03-06 13:29:53 +0000

    Login db test code

--- Register.php
+++ Register.php
@@ -1,25 +1,34 @@
 <?php
 require("db.php");
 
-if (isset($_REQUEST['user_name']) && isset($_REQUEST['password']) ) {
-    $uname = $_REQUEST['user_name'];
-    $upass = $_REQUEST['password'];
+if (isset($_REQUEST['username']) && isset($_REQUEST['pass'])
+&& isset($_REQUEST['email'])) {
+
+    $uname = $_REQUEST['username'];
+    $upass = $_REQUEST['pass'];
+    $email = $_REQUEST['email'];
 
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     } else {
         $uname = $conn->real_escape_string($uname);
         $upass = $conn->real_escape_string($upass);
+        $email = $conn->real_escape_string($email);
 
         $sql = "SELECT registered FROM user where username='{$uname}' AND password='{$upass}' LIMIT 1;";
 
         $result = $conn->query($sql);
         if ($result->num_rows > 0)
         {
-            $row=mysqli_fetch_row($result);
-            echo $row[0] ? "You logged and registered": "You are logged in but not registered";
+            echo "This username $uname is already taken";
         } else {
-            echo "Failed to log in";
+            mysqli_free_result($result);
+            $sql = "INSERT INTO user (email, username, password) VALUES('{$email}','{$uname}','{$upass}');";
+            if($conn->query($sql) === TRUE) {
+                echo "User inserted into the database";
+            } else {
+                echo "Failed to insert user into the database";
+            }
         }
     }
     $conn->close();
