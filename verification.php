25487875a15434a37e3019f86d170e0ee550d344
Paul Kinsella <paulkinselladev@gmail.com>
2020-03-07 01:15:56 +0000

    Add verification db logic

--- verification.php
+++ verification.php
@@ -1,1 +1,19 @@
 <?php
+require 'vendorv/autoload.php';
+require("db.php");
+
+if (isset($_GET['verification'])) {
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    } else {
+        // decrypt here
+        $decryptedEmail = "";
+        $sql = "UPDATE user SET regisitered = 1 WHERE email='{$decryptedEmail}'";
+
+        if ($conn->query($sql) === TRUE)
+        {
+            //display success message and redirect
+        }
+    }
+
+}
