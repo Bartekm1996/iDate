ffb64a3b2042de3a0d65deb0831c519e73345c11
Paul Kinsella <paulkinselladev@gmail.com>
2020-03-20 00:33:12 +0000

    Match Content view

--- TestUi.php
+++ TestUi.php
@@ -328,34 +328,7 @@
             </div>
             <div>list</div>
         </div>
-        <div class="col-md-8">
-            <div class="card shadow-lg p-3 mb-5 bg-white rounded" style="width: 25rem;display:block; margin-left: auto;margin-right: auto">
-                <div style="position:relative">
-                    <img id="imgP" width="300px" height="350px" src="https://placekitten.com/300/300" class="card-img-top">
-                    <div class="matchbtn" style="left:2%">
-                        <i class="fa fa-3x fa-arrow-circle-left" onclick="prevImg()"></i>
-                    </div>
-                    <div class="matchbtn" style="right:2%">
-                        <i class="fa fa-3x fa-arrow-circle-right" onclick="nextImg()"></i>
-                    </div>
-
-                </div>
-
-                <div class="card-body">
-                    <div style="position:relative;left: 35%;">
-                        <i class="fa fa-4x fa-ban" style="color: rgba(14,0,6,0.87);" onclick="match()"></i>
-                        <i class="fa fa-4x fa-heart" style="color: red;" onclick="nomatch()"></i>
-                    </div>
-                    <h5 class="card-title">Sally <e>23</e></h5>
-                    <h6><i class="fa fa-graduation-cap"></i> University Of Limerick</h6>
-                    <h6><i class="fa fa-suitcase"></i> Software Developer</h6>
-                    <h6><i class="fa fa-map-marker"></i> Co. Limerick</h6>
-                    </div>
-<!--                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>-->
-
-                </div>
-            </div>
-        </div>
+        <?php require("matchcontent.php"); ?>
     </div>
 </div>
 
