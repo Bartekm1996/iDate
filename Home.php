7b22389fb5c8944a455715e1076b7758338b5664
Bartlomiej Mlynarkiewicz <bmlynarkiewicz@hotmail.com>
2020-04-18 02:14:54 +0100

    Re-added omitted file sending email on delete and block

--- Home.php
+++ Home.php
@@ -593,8 +593,8 @@
 
             //$('<li class="sent"><img src="http://emilcarlsson.se/assets/mikeross.png" alt="" /><p>' + message + '</p></li>').appendTo($('.messages ul'));
             $('.message-input input').val(null);
-            $(".messages").animate({ scrollTop: $(document).height() }, "slow");
-            
+            $(".messages").animate({ scrollTop: $(window).height() }, "slow");
+
         };
 
 
